efdbc30cf019718d9a159b997ec7648a85daaac3
José Ronaldo <dev.joseronaldo@gmail.com>
2024-08-06 17:16:54 -0300

    feat: add card component

---
+++ mobile/src/utils/dateFormater.ts
@@ -0,0 +1,29 @@
+import { differenceInDays, differenceInHours, differenceInMonths, differenceInWeeks, differenceInYears, parseISO } from 'date-fns';
+
+export function timeForNow(dateString: string): string {
+  const date = parseISO(dateString);
+  const now = new Date();
+
+  const hours = differenceInHours(now, date);
+  if (hours < 48) {
+    return `há ${hours} hora${hours !== 1 ? 's' : ''} atrás`;
+  }
+
+  const days = differenceInDays(now, date);
+  if (days < 6) {
+    return `há ${days} dia${days !== 1 ? 's' : ''} atrás`;
+  }
+
+  const weeks = differenceInWeeks(now, date);
+  if (weeks < 4) {
+    return `há ${weeks} semana${weeks !== 1 ? 's' : ''} atrás`;
+  }
+
+  const months = differenceInMonths(now, date);
+  if (months < 12) {
+    return `há ${months} mês${months !== 1 ? 'es' : ''} atrás`;
+  }
+
+  const years = differenceInYears(now, date);
+  return `há {years} ano`;
+}
