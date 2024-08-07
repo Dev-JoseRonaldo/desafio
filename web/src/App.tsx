e27ec81a4e6a3d35c85d003df4ec1de713578df4
José Ronaldo <dev.joseronaldo@gmail.com>
2024-08-07 17:32:15 -0300

    chore: tailwind config

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,8 @@
-import './App.css'
-
 function App() {
   return (
-    <h1>News Explorer</h1>
+    <main className="flex w-screen h-screen p-4 items-top justify-center"> 
+      <h1 className="text-6xl font-bold">News Explorer</h1>
+    </main>
   )
 }
 
